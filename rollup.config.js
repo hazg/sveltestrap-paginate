b45404774e7cea6c8b149bc505ee2c98072eff42
Aleksandr Alksandrov <hazg@mail.ru>
2021-03-12 16:47:26 +0300

    u

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,49 @@
 import svelte from 'rollup-plugin-svelte'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import livereload from 'rollup-plugin-livereload'
+import resolve from '@rollup/plugin-node-resolve'
+import autoPreprocess from 'svelte-preprocess'
+import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import bundleSize from 'rollup-plugin-bundle-size'
+import analyze from 'rollup-plugin-analyzer'
 
-export default [
-	{
-		input: 'src/SveltestrapPagination.svelte',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			name: 'app',
-			file: 'example/bundle.js'
-		},
-		plugins: [
-			svelte({
-				dev: true,
-				css: css => {
-					css.write('example/bundle.css')
-				}
-			}),
-			resolve({
-				browser: true,
-				dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-			}),
-			commonjs(),
-			livereload('example')
-		],
-		watch: {
-			clearScreen: false
-		}
-	}
-]
+import pkg from './package.json'
+
+const production = !process.env.ROLLUP_WATCH
+
+const { name } = pkg
+
+export default {
+  input: 'src/SveltestrapPagination.svelte',
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: !production,
+      name
+    },
+    {
+      file: pkg.main,
+      format: 'umd',
+      sourcemap: !production,
+      name
+    }
+  ],
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // generate: production ? 'dom' : 'ssr',
+      hydratable: true,
+      preprocess: autoPreprocess({
+        postcss: {
+          plugins: [require('autoprefixer')()]
+        }
+      })
+    }),
+    resolve(),
+    commonjs(),
+    production && terser(),
+    production && analyze(),
+    production && bundleSize(),
+  ],
+}
